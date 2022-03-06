974bb987e62e7dc16138f877abf1d89032ffe029
YunA486 <w2039@e-mirim.hs.kr>
2022-03-07 01:22:46 +0900

    Edit css

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 function scrollUp(top) {
     const vheight = $('.test').height();
-    const margin_top = parseInt($('#survey').css('margin-top'), 10);
+    const margin_top = parseInt($('#survey').css('margin-top'), 25);
     $('html, body').animate({
         scrollTop: top - vheight - margin_top
     }, 500);
@@ -8,7 +8,7 @@ function scrollUp(top) {
 
 function scrollDown(top) {
     const vheight = $('.test').height();
-    const margin_top = parseInt($('#survey').css('margin-top'), 10);
+    const margin_top = parseInt($('#survey').css('margin-top'), 6);
     $('html, body').animate({
         scrollTop: vheight + top - margin_top
     }, 500);
@@ -18,13 +18,15 @@ $('.next_btn').click(function(e){
     let divs = $(this).parent().prev().children();
     let present_top = $(this).parent().parent()[0].offsetTop;
     let inputs = divs.find('input:checked');
-    if(inputs.length < 1) {
-        alert('문항이 선택되지 않았습니다.');
-        return false;
-    }
     e.preventDefault();
     scrollDown(present_top);
 });
+
+// if(inputs.length < 1) {
+    //     alert('문항이 선택되지 않았습니다.');
+    //     return false;
+    // }
+
 
 $('.prev_btn').click(function(e){
     let present_top = $(this).parent().parent()[0].offsetTop;
